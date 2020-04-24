ea225ecfc67d61673114424b7d408b65011616c9
jitwit <jrn@bluefarm.ca>
2020-04-24 14:32:21 -0400

    unittest

--- gnu/packages/j-xyz.scm
+++ gnu/packages/j-xyz.scm
@@ -200,82 +200,82 @@
       (description "Color tables and related scripts")
       (license expat))))
 
-;; ;; https://github.com/jsoftware/graphics_png.git
-;; ;; 2767c9b8efea71c38b0d8433bd58aba360ea464a
-;; (define-public j-graphics-png
-;;   (let ((commit "2767c9b8efea71c38b0d8433bd58aba360ea464a")
-;;         (version "1.0.28"))
-;;     (package
-;;       (name "j-graphics-png")
-;;       (version version)
-;;       (source
-;;        (origin
-;;          (method git-fetch)
-;;          (uri
-;;           (git-reference
-;;            (url "https://github.com/jsoftware/graphics_png.git")
-;;            (commit commit)))
-;;          (sha256
-;;           (base32 "1i5i9x7am36dr58bvlhydyp3bhmhbgg355k9jfddjylbrsnb7rc9"))))
-;;       (inputs `(("j" ,j-901)
-;;                 ("j-arc-zlib" ,j-arc-zlib)))
-;;       ;; (propagated-inputs `(("j-arc-zlib" ,j-arc-zlib)))
-;;       (outputs '("out"))
-;;       (build-system gnu-build-system)
-;;       (arguments
-;;        `(#:modules ((guix build gnu-build-system) (guix build utils))
-;;          #:tests? #f
-;;          #:phases
-;;          (modify-phases %standard-phases
-;;            (delete 'configure)
-;;            (delete 'check)
-;;            (delete 'build)
-;;            (replace 'install
-;;              (lambda _
-;;                (let* ((out (string-append (assoc-ref %outputs "out")
-;;                                           "/share/j/addons/graphics/png")))
-;;                  (install-file "png.ijs" out)
-;;                  #t))))))
-;;       (home-page "https://github.com/jsoftware/arc_zlib")
-;;       (synopsis "Interface with zlib")
-;;       (description "This J addon provides an interface to zlib.")
-;;       (license expat))))
-;; 
-;; ;; bbfc957fc4ddf90231c4a06239cbc85c67cc2769
-;; ;; https://github.com/jsoftware/general_misc.git
-;; (define j-general-misc
-;;   (let ((commit "bbfc957fc4ddf90231c4a06239cbc85c67cc2769")
-;;         (version "2.5.3"))
-;;     (package
-;;       (name "j-general-misc")
-;;       (version version)
-;;       (source
-;;        (origin
-;;          (method git-fetch)
-;;          (uri
-;;           (git-reference
-;;            (url "https://github.com/jsoftware/general_misc.git")
-;;            (commit commit)))
-;;          (sha256
-;;           (base32 "0pp71i7ylyn4nv43vagh0h2y1yb2dx0py97bscxykk04xavcvx6j"))))
-;;       (inputs `(("j" ,j-901)))
-;;       (build-system gnu-build-system)
-;;       (arguments
-;;        `(#:modules ((guix build gnu-build-system) (guix build utils))
-;;          #:tests? #f
-;;          #:phases
-;;          (modify-phases %standard-phases
-;;            (delete 'configure)
-;;            (delete 'check)
-;;            (delete 'build)
-;;            (replace 'install
-;;              (lambda _
-;;                (let* ((out (string-append (assoc-ref %outputs "out")
-;;                                           "/share/j/addons/graphics/png")))
-;;                  (install-file "png.ijs" out)
-;;                  #t))))))
-;;       (home-page "https://github.com/jsoftware/arc_zlib")
-;;       (synopsis "Interface with zlib")
-;;       (description "This J addon provides an interface to zlib.")
-;;       (license expat))))
-;; 
+;; https://github.com/jsoftware/graphics_png.git
+;; 2767c9b8efea71c38b0d8433bd58aba360ea464a
+(define-public j-graphics-png
+  (let ((commit "2767c9b8efea71c38b0d8433bd58aba360ea464a")
+        (version "1.0.28"))
+    (package
+      (name "j-graphics-png")
+      (version version)
+      (source
+       (origin
+         (method git-fetch)
+         (uri
+          (git-reference
+           (url "https://github.com/jsoftware/graphics_png.git")
+           (commit commit)))
+         (sha256
+          (base32 "1i5i9x7am36dr58bvlhydyp3bhmhbgg355k9jfddjylbrsnb7rc9"))))
+      (inputs `(("j" ,j-901)
+                ("j-arc-zlib" ,j-arc-zlib)))
+      (outputs '("out"))
+      (build-system gnu-build-system)
+      (arguments
+       `(#:modules ((guix build gnu-build-system) (guix build utils))
+         #:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure)
+           (delete 'check)
+           (delete 'build)
+           (replace 'install
+             (lambda _
+               (let* ((out (string-append (assoc-ref %outputs "out")
+                                          "/share/j/addons/graphics/png")))
+                 (copy-recursively "." out)
+                 #t))))))
+      (home-page "https://github.com/jsoftware/graphics_png")
+      (synopsis "")
+      (description "This J addon provides an interface to zlib.")
+      (license expat))))
+
+;; https://github.com/jsoftware/general_unittest.git
+;; 1.0.12
+;; c0916768bba3832fbb9bf2305d34575a5adc0ad0
+(define-public j-general-unittest
+  (let ((commit "c0916768bba3832fbb9bf2305d34575a5adc0ad0")
+        (version "1.0.12"))
+    (package
+      (name "j-general-unittest")
+      (version version)
+      (source
+       (origin
+         (method git-fetch)
+         (uri
+          (git-reference
+           (url "https://github.com/jsoftware/general_unittest.git")
+           (commit commit)))
+         (sha256
+          (base32 "0j78h07jm1b1q79vz091kv54cqx7g1jralj1dy5vyvswj3gkqrwj"))))
+      (native-inputs `(("j" ,j-901)))
+      (outputs '("out"))
+      (build-system gnu-build-system)
+      (arguments
+       `(#:modules ((guix build gnu-build-system) (guix build utils))
+         #:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure)
+           (delete 'check)
+           (delete 'build)
+           (replace 'install
+             (lambda _
+               (let* ((out (string-append (assoc-ref %outputs "out")
+                                          "/share/j/addons/general/unittest")))
+                 (copy-recursively "." out)
+                 #t))))))
+      (home-page "https://github.com/jsoftware/general_unittest")
+      (synopsis "Unit Test Framework")
+      (description "Implements a unit test framework.")
+      (license expat))))
